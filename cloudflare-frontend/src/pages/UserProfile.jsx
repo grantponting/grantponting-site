fdfab096fb26fd598b218aa739bb178a5c7951d5
grantponting <grantponting25@gmail.com>
2025-04-15 18:19:46 -0600

    Update import

--- cloudflare-frontend/src/pages/UserProfile.jsx
+++ cloudflare-frontend/src/pages/UserProfile.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import * as api from '../api-client/api';
-import axiosInstance from '../authorizedApi';
+import axiosInstance from '../utils/authorizedApi';
 
 
 const UserProfile = () => {
