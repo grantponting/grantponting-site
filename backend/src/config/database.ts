a96623c8247a5666a29f08c7638de3a5c272dc6f
grantponting <grant@platformsocialorderoutlook.onmicrosoft.com>
2026-06-18 13:40:54 -0600

    Cleanup repo

--- backend/src/config/database.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaD1 } from "@prisma/adapter-d1";
-import type { Env } from "../app"; // Import types if you have them defined
-
-export const createPrismaClient = (env: Env) => {
-    const adapter = new PrismaD1(env.DB);
-    return new PrismaClient({ adapter });
-};
